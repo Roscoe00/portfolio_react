c89d2e068b7a507eacfae316abd0c116becf4d1e
Roscoe Bull <roscoe-bull@live.co.uk>
2021-12-30 17:49:55 +0000

    home page finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.scss';
-import Navbar from './components/Navbar/Navbar';
+import About from './components/About/About';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Home />
+      <About />
     </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import "./Navbar.scss"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = () => {
    return (
       <nav className="nav">
          <h2 className="nav__name">R.Bull</h2>
          <div className="nav__links">
-            {/* <img className="nav__links-icon" src="" alt="nav menu" /> */}
-            <span className="nav__links-icon">&9776</span>
-            <span><a className="nav__links-title" href="About"> About Me</a></span>
-            <span><a className="nav__links-title" href="Experience">Experience</a></span>
-            <span><a className="nav__links-title" href="Projects">Projects</a></span>
-            <span><a className="nav__links-title" href="Contact">Contact</a></span>
+            <span className="nav__links-icon"><FontAwesomeIcon className='icon' icon={faBars} /></span>
+            <span><a className="nav__links-title" href="about"> About Me</a></span>
+            <span><a className="nav__links-title" href="experience">Experience</a></span>
+            <span><a className="nav__links-title" href="projects">Projects</a></span>
+            <span><a className="nav__links-title" href="contact">Contact</a></span>
          </div>
       </nav>
    )
