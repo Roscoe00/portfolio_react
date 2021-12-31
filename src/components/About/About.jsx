ee4214291558e57af9d7d9df934a277530689ff4
Roscoe Bull <roscoe-bull@live.co.uk>
2021-12-31 19:24:01 +0000

    made changes to home page, added content for about page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 const About = () => {
    return (
-      <div>
-
+      <div className="about">
+         <h2>About Me</h2>
+         <div className="about__grid">
+            <p className="about__paragraph-one"> Computers have been a huge part of my life so it was only natural for my job to be computer based. </p>
+            <p className="about__paragraph-two">
+               My aim is to work with a dedicated team in an environment where I have plenty of room to evolve and develop both my skills, and as a person.
+            </p>
+            <img className="about__image" src="" alt="me" />
+         </div>
       </div>
    )
 }
