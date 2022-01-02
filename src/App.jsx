cb9b2801ea197b22c3a9295014da6541eb7557e2
Roscoe Bull <roscoe-bull@live.co.uk>
2022-01-02 18:20:02 +0000

    experience page and svgs added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.scss';
 import About from './components/About/About';
+import Experience from './components/Experience/Experience';
 import Home from './components/Home/Home';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Home />
       <About />
+      <Experience />
     </div>
   );
 }
--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
+import "./About.scss"
+import image from "../../assets/images/about_me.jpg"
 
 const About = () => {
    return (
       <div className="about">
-         <h2>About Me</h2>
+         <h2 className="about__title">About Me</h2>
          <div className="about__grid">
-            <p className="about__paragraph-one"> Computers have been a huge part of my life so it was only natural for my job to be computer based. </p>
+            <p className="about__paragraph-one"> Computers have been a huge part of my life ever since my family got a windows 95 PC forever ago. Growing up with computers, they were a natural interest in my life and have ultimately become an area I want to develop a career in.</p>
             <p className="about__paragraph-two">
-               My aim is to work with a dedicated team in an environment where I have plenty of room to evolve and develop both my skills, and as a person.
+               I decided to specifically persue coding and software development after finishing a degree in mathematics, and really enjoying using programs like MatLAB and R Studio. I joined the Technology Consultancy Programme ran by _nology in order to gain the skills I needed to become a Software Engineer.
             </p>
-            <img className="about__image" src="" alt="me" />
+            <img className="about__image" src={image} alt="me" />
          </div>
       </div>
    )
